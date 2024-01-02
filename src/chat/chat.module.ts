0598b2a6b2d484b40e089c2fd2c0d31d1dfcf47e
Никита Стратий <nikita@Noutbuk-Nikita.local>
2024-01-02 22:23:16 +0300

    add simple socket gateway

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -3,11 +3,13 @@ import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Chat } from './chat.entity';
+import { ChatGateway } from './chat.gateway';
+import { MessageModule } from '../message/message.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Chat])],
+  imports: [TypeOrmModule.forFeature([Chat]), MessageModule],
   controllers: [ChatController],
-  providers: [ChatService],
+  providers: [ChatService, ChatGateway],
   exports: [],
 })
 export class ChatModule {}
